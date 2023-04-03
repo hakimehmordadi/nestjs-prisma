e2393bfc791eaa67c9e382f7f3e2c6ce9a041ae6
Golnar Mordadi <golnarmordadi@gmail.com>
2023-04-03 05:18:01 +0300

    :memo: Updated for delete

--- src/loans/loans.service.ts
+++ src/loans/loans.service.ts
@@ -6,28 +6,30 @@ import { PrismaService } from '../prisma/prisma.service';
 export class LoansService {
   constructor(private prisma: PrismaService) {}
 
-  loanBatchOperation(loans: CreateLoanDto[]) {
+  async loanBatchOperation(loans: CreateLoanDto[]) {
     if (!loans) {
       throw new Error(`Invalid loan object: ${JSON.stringify(loans)}`);
     }
     try {
+      await this.prisma.loan.deleteMany({
+        where: {
+          id: {
+            notIn: loans.map((loan) => loan.id)
+          }
+        }
+      })
+  
       loans.map(async (loan) => {
-        
-        /** Delete is commented because some times not works properly */
-
-        // await this.prisma.loan.deleteMany({
-        //     where: {
-        //       id: {
-        //         not: loan.id,
-        //       }
-        //     },
-        // });
         await this.prisma.loan.upsert({
             where: { id: loan.id },
             update: loan,
             create: loan,
         });
       })
+    } catch (error) {
+      throw new Error(`Loan batch operation runs with an error: ${JSON.stringify(error)}`) ;
+    } 
+  }
     } catch (error) {
       throw new Error(`Loan batch operation runs with an error: ${JSON.stringify(error)}`) ;
     } 
